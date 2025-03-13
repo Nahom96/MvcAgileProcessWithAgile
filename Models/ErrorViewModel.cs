db3647b347e16320c3d14c6646edb5a8104dc42d
Nahom Araya <naraya2@my.centennialcollege.ca>
2025-03-13 00:50:17 -0400

    Initial commit - ASP.NET Core MVC setup

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MvcAgileProcess.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
